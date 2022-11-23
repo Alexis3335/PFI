77019d981fc93da9469ea6fb025ca98147f34cd4
Alexis3335 <95308206+Alexis3335@users.noreply.github.com>
2022-11-23 17:33:12 -0500

    login fini

--- app/src/main/java/com/example/pfi/login.java
+++ app/src/main/java/com/example/pfi/login.java
@@ -2,10 +2,12 @@ package com.example.pfi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
@@ -17,7 +19,17 @@ public class login extends AppCompatActivity {
         Button btnLogin = findViewById(R.id.btnLogin);
         EditText editTextNom = findViewById(R.id.editLoginNom);
         EditText editTextPass = findViewById(R.id.editLoginPass);
+        TextView erreur = findViewById(R.id.textLoginErreur);
         btnLogin.setOnClickListener(view -> {
+            String nom = editTextNom.getText().toString();
+            String pass = editTextPass.getText().toString();
+            if(nom.isEmpty() || pass.isEmpty()){
+                erreur.setVisibility(View.VISIBLE);
+            }
+            else{
+                Clients client = new Clients(nom);
+               // Intent intent = new Intent
+            }
 
         });
     }
