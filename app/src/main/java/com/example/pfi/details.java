a80d6cb14bee6e3962262f76dd3ff3b9940424d0
leatg <202140783@edu.clg.qc.ca>
2022-11-25 15:16:54 -0500

    databinding

--- app/src/main/java/com/example/pfi/details.java
+++ app/src/main/java/com/example/pfi/details.java
@@ -1,14 +1,23 @@
 package com.example.pfi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import com.example.pfi.databinding.ActivityDetailsBinding;
 
 public class details extends AppCompatActivity {
+
+    ActivityDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        //getIntent()
+
+        binding = DataBindingUtil.setContentView(details.this, R.layout.activity_details);
+        //binding.
     }
 }
