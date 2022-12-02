c22b81ac2e6342ad0d8b3ebe11c62e8b3ebad565
Alexis3335 <95308206+Alexis3335@users.noreply.github.com>
2022-12-02 15:07:29 -0500

    animation

--- app/src/main/java/com/example/pfi/panier.java
+++ app/src/main/java/com/example/pfi/panier.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.Serializable;
@@ -25,14 +26,43 @@ public class panier extends AppCompatActivity implements Serializable {
 
         Button retour = (Button) findViewById(R.id.btnRetourPanier);
         RecyclerView recycler = findViewById(R.id.recyclerPanier);
+        boolean estPanier = true;
+
+        Adaptateur adaptateur = new Adaptateur(getApplicationContext(),panier, estPanier);
+
+        TextView nomprenom = findViewById(R.id.prenomnom);
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                char[] nom = client.getNom().toCharArray();
+                while(true){
+                    for(char c : nom){
+                        String curr = nomprenom.getText().toString() + c;
+                        nomprenom.setText(curr);
+
+                        try {
+                            Thread.sleep(250);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        if(curr.equals(client.getNom()))
+                            nomprenom.setText("");
+                    }
+                }
+            }
+        });
+
+        th.start();
 
 
-        Adaptateur adaptateur = new Adaptateur(getApplicationContext(),panier);
         adaptateur.client = client;
         recycler.setAdapter(adaptateur);
 
         recycler.setLayoutManager(new LinearLayoutManager(this));
         recycler.setItemAnimator(new DefaultItemAnimator());
         recycler.setHasFixedSize(true);
+
+
     }
 }
