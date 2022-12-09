19c917d7c110c05564ae4c51e8c012df81d4f817
Alexis3335 <95308206+Alexis3335@users.noreply.github.com>
2022-12-09 13:27:31 -0500

    son

--- app/src/main/java/com/example/pfi/PageFin.java
+++ app/src/main/java/com/example/pfi/PageFin.java
@@ -3,6 +3,7 @@ package com.example.pfi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,9 @@ public class PageFin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_fin);
+
+        MediaPlayer sonPayer = MediaPlayer.create(this,R.raw.son_payer_panier);
+        sonPayer.start();
 
         Button btnRetour = findViewById(R.id.btnRetourFin);
         Clients client = (Clients)getIntent().getSerializableExtra("client");
